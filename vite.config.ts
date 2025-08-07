4e039d3cc7705fcd13fbf4b57fc0380a879a34e3
Andreas Schlegel <andreas.schlegel@mgb.ch>
2025-08-07 12:47:10 +0200

    Setup for react testing library.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      // @ts-ignore
-      tailwindcss({}),
-  ],
+    plugins: [
+        react(),
+        // @ts-ignore
+        tailwindcss({}),
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTests.ts',
+    },
 })
