66e02233d8e813f35b691ba0f10163064d61bd0f
Andreas Schlegel <andreas.schlegel@mgb.ch>
2025-08-11 11:18:43 +0200

    Moved Overlay to separate folder and implemented tests for the CookieOverlay.

--- src/components/CookieOverlay/CookieOverlay.tsx
+++ src/components/CookieOverlay/CookieOverlay.tsx
@@ -5,14 +5,14 @@ export default function CookieOverlay({visible}: { visible: boolean }) {
     
     useEffect(() => {
         if (visible) setShow(true);
-        else setTimeout(() => setShow(false), 300); // Fade-out delay
+        else setTimeout(() => setShow(false), 300);
     }, [visible]);
     
     return show ? (
-        <div
+        <div data-testid="cookie-overlay"
             className={`
         fixed inset-0 bg-black transition-opacity duration-300 z-99
-        ${visible ? "opacity-50 pointer-events-auto" : "opacity-0 pointer-events-none"}
+        ${visible ? "opacity-50 pointer-events-none" : "opacity-0 pointer-events-auto"}
       `}
         />
     ) : null;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useState } from "react";
-import CookieOverlay from "../components/CookieOverlay.tsx";
+import CookieOverlay from "../components/CookieOverlay/CookieOverlay.tsx";
 import CookieBanner from "../components/CookieBanner/CookieBanner.tsx";
 import { useCookieConsent } from "../hooks/useCookieConsent.ts";
 
