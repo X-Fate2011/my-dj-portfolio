20093321d90417abc670d3f081573abcbf74df6f
Andreas Schlegel <andreas.schlegel@mgb.ch>
2025-08-25 23:54:33 +0200

    Localized homepage content by adding new translation keys for subtitles and descriptions, updated JSON locale files, and integrated translations into the `App` component. Reworked mobile styling

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,16 +4,15 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 import MixcloudFeedHome from "../components/MixcloudFeedHome.tsx";
 import TwitchStream from "../components/TwitchStream.tsx";
+import { useTranslation } from "react-i18next";
 
 function App() {
-    
+    const { t } = useTranslation("common");
     return (
         <>
             <section className="flex flex-col">
-                <div className="flex flex-col lg:flex-row mb-4 lg:m-8 h-[450px] md:h-[350px] lg:h-[450px]">
-                    {/* Overlay */}
-                    
-                    <div className="relative w-full overflow-hidden">
+                <div className="relative overflow-hidden flex flex-col lg:flex-row mb-4 lg:m-8 md:h-[350px] lg:h-[450px]">
+                    <div className="absolute lg:relative w-full overflow-hidden bg-white/75 lg:rounded-2xl">
                         <picture>
                             <source
                                 srcSet="/x-fate_480.webp 480w, /x-fate_1280.webp 1280w, /x-fate.webp 1920w"
@@ -32,32 +31,23 @@ function App() {
                     </div>
                     
                     {/* Content */}
-                    <div className="flex flex-col absolute lg:relative justify-between lg:ml-8 z-20">
+                    <div className="flex flex-col relative justify-between lg:ml-8 z-20">
                         <div className="p-4 lg:p-6 xl:p-8 lg:mb-8 bg-transparent lg:bg-white lg:rounded-2xl text-black max-w-full">
-                            <h2>Sound. Energie. Emotion. Passion.</h2>
-                            <p className="text-base lg:text-lg xl:text-xl">
-                                Mein Name ist X-Fate und ich mixe seit 2011 elektronische Musik – von den ersten Beats im Webradio über
-                                Festivalbühnen
-                                bis hin zu meinen aktuellen Livestreams auf Twitch.
-                                Ob EDM, Hardstyle, Hard-/Frenchcore oder Drum & Bass: Ich liebe es, Genres zu verbinden und Menschen mit
-                                Energie
-                                und Emotionen zu begeistern.
-                                Hör rein, tauch ein – und folg mir auf meiner musikalischen Reise!
-                            </p>
+                            <h2>{t("home.subtitle1")}</h2>
+                            <p className="text-sm sm:text-base lg:text-lg xl:text-xl">{t("home.subtext1")}</p>
                         </div>
                         
-                        <div className="p-4 lg:p-6 xl:p-8 bg-transparent lg:bg-white lg:rounded-2xl text-black max-w-full z-20">
-                            <h2>Kommende Shows</h2>
-                            <p className="text-base lg:text-lg xl:text-xl">
-                                Jeden Dienstag um 20:00 Uhr – im Wechsel Webradio &amp; Twitch.
-                                Schau vorbei und sei live dabei!
+                        <div className="px-4 pb-4 lg:p-6 xl:p-8 bg-transparent lg:bg-white lg:rounded-2xl text-black max-w-full z-20">
+                            <h2>{t("home.subtitle2")}</h2>
+                            <p className="text-sm sm:text-base lg:text-lg xl:text-xl">
+                                {t("home.subtext2")}
                             </p>
-                            <p className="text-black text-base lg:text-lg xl:text-xl">
+                            <p className="text-black text-sm sm:text-base lg:text-lg xl:text-xl">
                                 <a href="https://twitch.tv/truededicationmusic" target="_blank" rel="noopener noreferrer"
-                                   className="!text-black"><FontAwesomeIcon icon={faTwitch}/>Twitch</a>
+                                   className="!text-black"><FontAwesomeIcon icon={faTwitch}/>{t("home.twitchAnchor")}</a>
                                 &nbsp;|&nbsp;
                                 <a href="https://rm.fm/house" target="_blank" rel="noopener noreferrer"
-                                   className="!text-black"><FontAwesomeIcon icon={faPlay}/>RauteMusik</a>
+                                   className="!text-black"><FontAwesomeIcon icon={faPlay}/>{t("home.rmAnchor")}</a>
                             </p>
                         </div>
                     </div>
