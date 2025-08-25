469106d463881de5398aa1ba04832451742f3ee1
Andreas Schlegel <andreas.schlegel@mgb.ch>
2025-08-25 23:17:58 +0200

    Localized `MixcloudFeedHome` and `TwitchStream` components by integrating translations and updated JSON locale files accordingly.

--- src/components/MixcloudFeedHome.tsx
+++ src/components/MixcloudFeedHome.tsx
@@ -1,20 +1,18 @@
 import MixcloudPlayer from "./MixcloudPlayer/MixcloudPlayer.tsx";
 import { useCookieConsent } from "../hooks/useCookieConsent.ts";
+import { useTranslation } from "react-i18next";
 
 export default function MixcloudFeedHome() {
     const consentGiven = useCookieConsent();
+    const { t } = useTranslation("common");
     
     return (
         <section aria-label="Letzte Shows" className="z-10 p-4 lg:p-0 lg:ml-8 w-full lg:w-1/2">
-            <h2>Die letzten Shows nochmal anhören</h2>
+            <h2>{ t("home.mixcloudFeedHomeTitle") }</h2>
             <div className="grid grid-cols-1 gap-6">
-                {consentGiven ? (
-                    <MixcloudPlayer limit={2} variant="compact"/>
-                    ) : (
-                        <>
-                            <p>Bitte akzeptiere die Cookies, um den Mixcloud-Player nutzen zu können.</p>
-                        </>
-                    )}
+                {
+                    consentGiven ? <MixcloudPlayer limit={2} variant="compact"/> : <p>{ t("home.mixcloudFeedHomeCookieNote") }</p>
+                }
             </div>
         </section>
     );
