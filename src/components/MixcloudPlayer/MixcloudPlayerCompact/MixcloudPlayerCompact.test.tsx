66da2abbb1494038c1ad0103232ab2e0e8396110
Andreas Schlegel <andreas.schlegel@mgb.ch>
2025-08-26 11:33:15 +0200

    Wrapped `MixcloudPlayerCompact` tests with `MemoryRouter` to support components relying on `react-router-dom`. Updated expectations in test cases accordingly.

--- src/components/MixcloudPlayer/MixcloudPlayerCompact/MixcloudPlayerCompact.test.tsx
+++ src/components/MixcloudPlayer/MixcloudPlayerCompact/MixcloudPlayerCompact.test.tsx
@@ -3,6 +3,7 @@ import { MixcloudPlayerCompact } from "./MixcloudPlayerCompact";
 import { MixcloudPlayerFrame } from "../MixcloudPlayerFrame/MixcloudPlayerFrame";
 import { describe, it, expect as vitestExpect } from "vitest";
 import { MOCK_DATA } from "../../../mock/mixcloud-mock";
+import { MemoryRouter } from "react-router-dom";
 
 vi.mock("../MixcloudPlayerFrame/MixcloudPlayerFrame", () => ({
     MixcloudPlayerFrame: vi.fn(() => null)
@@ -10,24 +11,24 @@ vi.mock("../MixcloudPlayerFrame/MixcloudPlayerFrame", () => ({
 
 describe("MixcloudPlayerCompact", () => {
     it("renders nothing when variant is not 'compact'", () => {
-        const { container } = render(<MixcloudPlayerCompact variant={"list"} shows={[]} />);
+        const { container } = render(<MemoryRouter><MixcloudPlayerCompact variant={"list"} shows={[]} /></MemoryRouter>);
         vitestExpect(container).toBeEmptyDOMElement();
     });
     
     it("renders a container div with grid styling when variant is 'compact'", () => {
-        render(<MixcloudPlayerCompact variant={"compact"} shows={[]} />)
+        render(<MemoryRouter><MixcloudPlayerCompact variant={"compact"} shows={[]} /></MemoryRouter>)
         const wrapper = screen.getByTestId("mixcloudPlayerFrameWrapper");
         expect(wrapper).toHaveClass("grid", { exact: false });
     });
     
     it("renders one MixcloudPlayerFrame per show item", () => {
-        render(<MixcloudPlayerCompact variant="compact" shows={MOCK_DATA} />);
+        render(<MemoryRouter><MixcloudPlayerCompact variant="compact" shows={MOCK_DATA} /></MemoryRouter>);
         expect(MixcloudPlayerFrame).toHaveBeenCalledTimes(MOCK_DATA.length);
     });
     
-    it("renders no children when shows array is empty", () => {
-        render(<MixcloudPlayerCompact variant="compact" shows={[]} />);
+    it("renders no Mixcloud player frames when shows array is empty", () => {
+        render(<MemoryRouter><MixcloudPlayerCompact variant="compact" shows={[]} /></MemoryRouter>);
         const wrapper = screen.getByTestId("mixcloudPlayerFrameWrapper");
-        expect(wrapper).toBeEmptyDOMElement();
+        expect(wrapper.childElementCount).toBe(1);
     });
 });
